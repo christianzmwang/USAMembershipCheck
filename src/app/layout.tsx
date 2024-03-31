7f0ad7be6a71e4ecc776a961d721e1e960faa32c
justwph <justwph@gmail.com>
2024-03-31 15:41:29 -0700

    add trace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {Toaster} from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <Analytics/>
       <body className={`${inter.className} h-full w-full bg-subtle`}>{children}</body>
       <Toaster duration={60000} closeButton />
     </html>
