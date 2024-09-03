ae98798a44e39597eb5a2062ea43313fb975f528
Christian Wang <christianwang39@gmail.com>
2024-09-02 22:56:56 -0400

    general update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function MainComponent() {
 
   const locations = [
     { id: "pleasanton", name: "Pleasanton", address: "5870 Stoneridge Dr Suite 6, Pleasanton"},
-    { id: "los-gatos", name: "Los Gatos", address: "15445 Los Gatos Blvd, Los Gatos"}
+    { id: "los-gatos", name: "Los Gatos", address:   "15445 Los Gatos Blvd, Los Gatos"}
   ];
 
   const [location, setLocation] = useState("Los Gatos");
@@ -30,9 +30,9 @@ export default function MainComponent() {
         className="main-grid bg-default dark:bg-muted grid max-w-full items-start dark:[color-scheme:dark] sm:motion-reduce:transition-none md:flex-row rounded-md  sm:transition-[width] sm:duration-300 h-auto bg-background">
         <div className="p-4	 relative z-10 flex flex-col [grid-area:meta] border-b border--bsubtle" style={{height: "auto"}}>
           <h2 className="text-2xl  leading-relaxed"> Free Introductory Class </h2>
-          <div className="mt-2">
+          <div className="mt-2 mb-2">
             <div style={{ height: "auto" }} className="main-grid bg-default dark:bg-muted grid max-w-full items-start dark:[color-scheme:dark] sm:motion-reduce">
-              <div className="mb-4">
+              <div>
                 <label htmlFor="location" className="block text-sm font-medium text-gray-700">
                   Select Location
                 </label>
@@ -52,10 +52,12 @@ export default function MainComponent() {
               </div>
             </div>
           </div>
-          <p className="text-sm  leading-relaxed">
-            Book a free introductory class with us!<br/>
-            Address: <span className="underline">{selectedLocation?.address}</span>
-          </p>
+          <div className="flex">
+            <p className="text-sm  leading-relaxed">
+              Book a free introductory class with us!<br/>
+              Address: <span className="underline inline-block w-65">{selectedLocation?.address}</span>
+            </p>
+          </div>
         </div>
         <div className="calender-wrapper  [grid-area:main] md:border--bsubtle ml-[-1px] h-full flex-shrink px-2 py-3 md:border-r lg:w-[var(--booker-main-width)]">
           <Calender date={date} onDateChange={setDate}/>
